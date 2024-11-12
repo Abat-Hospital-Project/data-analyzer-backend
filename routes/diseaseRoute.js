04cfe5d340f3d629c1815ddc450f554116b385c0
Beamlak Tesfahun <beamlaktsahilu@gamil.com>
2024-11-12 14:27:58 +0300

    included imports for fetching diseases

--- routes/diseaseRoute.js
+++ routes/diseaseRoute.js
@@ -2,8 +2,12 @@ import express from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
 import {
   createDisease,
+  deleteDisease,
   diseaseOutcome,
   diseaseSymptom,
+  getAllDiseases,
+  getDisease,
+  updateDisease,
 } from "../controllers/diseaseController.js";
 import {
   handleValidationErrors,
@@ -15,5 +19,9 @@ const router = express.Router();
 router.post("/create", createDisease);
 router.post("/associate-symptom", authMiddleware, diseaseSymptom);
 router.post("/associate-outcome", authMiddleware, diseaseOutcome);
+router.get("/get-all", authMiddleware, getAllDiseases);
+router.get("/get/:diseaseId", authMiddleware, getDisease);
+router.put("/update/:diseaseId", authMiddleware, updateDisease);
+router.delete("/delete/:diseaseId", authMiddleware, deleteDisease);
 
 export default router;
